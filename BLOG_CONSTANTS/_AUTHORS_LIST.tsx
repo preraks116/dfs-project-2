9be2bc05ab07fa944103783b74b3f03364e72488
phish432 <rawat.agrim432@gmail.com>
2023-12-02 00:40:06 +0530

    Made new authors, new ebooks, new articles and linked to viewer

--- BLOG_CONSTANTS/_AUTHORS_LIST.tsx
+++ BLOG_CONSTANTS/_AUTHORS_LIST.tsx
@@ -1,10 +1,10 @@
 import { IAuthor } from "../src/shared/interfaces";
-import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 /**
  * EXAMPLE AUTHOR
  * 
- export const AUTHOR_NAME: IAuthor = {
+  AUTHOR_NAME: IAuthor = {
     name: "Full Name",
     designation: "Work Designation",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -23,63 +23,99 @@ import { AiFillLinkedin } from "react-icons/ai";
  */
 
 export const AUTHORS: { [key: string]: IAuthor } = {
-  SMITH: {
-    name: "Dr. Alice Smith",
+  JOHN: {
+    name: "Dr. John Smith",
     designation: "Oncologist",
-    bio: "Dr. Alice Smith is a dedicated oncologist with a passion for providing personalized cancer care. Her research focuses on innovative treatment approaches and improving patient outcomes.",
-    profilePic: "path/to/alice_smith_pic.jpg",
+    bio: "Dr. John Smith is a renowned oncologist with expertise in breast cancer research. He has dedicated his career to advancing the field of oncology and improving cancer treatment outcomes.",
+    profilePic: "",
     social: [
       {
         icon: <AiFillLinkedin />,
-        link: "https://www.linkedin.com/in/dr-alice-smith/",
+        link: "https://www.linkedin.com/in/drjohnsmith/",
+      },
+      {
+        icon: <AiFillGithub />,
+        link: "https://github.com/drjohnsmith",
       },
     ],
   },
-  JOHNSON: {
-    name: "Dr. Bob Johnson",
+  JANE: {
+    name: "Dr. Jane Doe",
     designation: "Cancer Researcher",
-    bio: "Dr. Bob Johnson is a leading cancer researcher specializing in molecular biology and genetics. His work aims to unravel the complexities of cancer at the genetic level to develop targeted therapies.",
-    profilePic: "path/to/bob_johnson_pic.jpg",
+    bio: "Dr. Jane Doe is a leading cancer researcher specializing in lung cancer. Her groundbreaking research has contributed significantly to our understanding of lung cancer biology and potential therapeutic interventions.",
+    profilePic: "",
     social: [
       {
         icon: <AiFillLinkedin />,
-        link: "https://www.linkedin.com/in/dr-bob-johnson/",
+        link: "https://www.linkedin.com/in/drjanedoe/",
       },
     ],
   },
-  DAVIS: {
+  MICHAEL: {
+    name: "Dr. Michael Johnson",
+    designation: "Pathologist",
+    bio: "Dr. Michael Johnson is a pathologist with a focus on gastrointestinal cancers. His expertise in histopathology has been instrumental in diagnosing and understanding various types of gastrointestinal malignancies.",
+    profilePic: "",
+    social: [
+      {
+        icon: <AiFillLinkedin />,
+        link: "https://www.linkedin.com/in/drmichaeljohnson/",
+      },
+      {
+        icon: <AiFillGithub />,
+        link: "https://github.com/drmichaeljohnson",
+      },
+    ],
+  },
+  SARAH: {
+    name: "Dr. Sarah Williams",
+    designation: "Oncologist",
+    bio: "Dr. Sarah Williams is a dedicated oncologist specializing in hematologic malignancies. Her commitment to patient care and research has contributed to advancements in the treatment of blood cancers.",
+    profilePic: "",
+    social: [
+      {
+        icon: <AiFillLinkedin />,
+        link: "https://www.linkedin.com/in/drsarahwilliams/",
+      },
+    ],
+  },
+  ALEX: {
+    name: "Dr. Alex Morgan",
+    designation: "Researcher",
+    bio: "Dr. Alex Morgan is a cancer researcher with a focus on genetic factors influencing cancer susceptibility. Her work has shed light on the intricate relationship between genetics and cancer development.",
+    profilePic: "",
+    social: [
+      {
+        icon: <AiFillLinkedin />,
+        link: "https://www.linkedin.com/in/dralexmorgan/",
+      },
+    ],
+  },
+  EMMA: {
     name: "Dr. Emma Davis",
-    designation: "Cancer Biologist",
-    bio: "Dr. Emma Davis is a dedicated cancer biologist specializing in understanding the tumor microenvironment. Her research contributes to the development of immunotherapies and targeted interventions.",
-    profilePic: "path/to/emma_davis_pic.jpg",
+    designation: "Pathologist",
+    bio: "Dr. Emma Davis is a pathologist specializing in breast cancer pathology. Her expertise in diagnosing and characterizing breast cancer tissues has played a crucial role in patient care and treatment planning.",
+    profilePic: "",
     social: [
       {
         icon: <AiFillLinkedin />,
-        link: "https://www.linkedin.com/in/dr-emma-davis/",
+        link: "https://www.linkedin.com/in/dremmadavis/",
       },
     ],
   },
-  WILSON: {
-    name: "Dr. John Wilson",
-    designation: "Clinical Oncologist",
-    bio: "Dr. John Wilson is a clinical oncologist dedicated to providing compassionate care to cancer patients. His research interests include clinical trials and improving the quality of life for cancer survivors.",
-    profilePic: "path/to/john_wilson_pic.jpg",
+  WILLIAM: {
+    name: "Dr. William Thompson",
+    designation: "Oncologist",
+    bio: "Dr. William Thompson is an oncologist with a focus on gastrointestinal malignancies. His clinical expertise and research contributions have enhanced our understanding and treatment options for digestive system cancers.",
+    profilePic: "",
     social: [
       {
         icon: <AiFillLinkedin />,
-        link: "https://www.linkedin.com/in/dr-john-wilson/",
+        link: "https://www.linkedin.com/in/drwilliamthompson/",
       },
-    ],
-  },
-  HARRIS: {
-    name: "Dr. Sarah Harris",
-    designation: "Cancer Immunologist",
-    bio: "Dr. Sarah Harris is a cancer immunologist dedicated to understanding the immune response to cancer. Her research focuses on developing immunotherapies to enhance the body's ability to fight cancer.",
-    profilePic: "path/to/sarah_harris_pic.jpg",
-    social: [
       {
-        icon: <AiFillLinkedin />,
-        link: "https://www.linkedin.com/in/dr-sarah-harris/",
+        icon: <AiFillGithub />,
+        link: "https://github.com/drwilliamthompson",
       },
     ],
   },
---
+++ pages/blog/ebook-2-cancer-chronicles/article-LU010.tsx
@@ -0,0 +1,58 @@
+import { PageLayout, Text, List, Image, LinkTo } from "../../../src/components";
+import { ImageSize } from "../../../src/shared/enums";
+
+const Article = () => {
+  return (
+    <PageLayout blogcentered>
+      <Image src="/public/images_histo/10.png" size={ImageSize.SMALL} alt="" />
+      <LinkTo
+        href="https://histopathology-viewer.vercel.app/viewer10.html"
+        passHref
+        className="flex items-center justify-center rounded-md bg-blue-600 px-3 pb-3 my-2 text-white hover:text-white shadow-lg hover:shadow-none transition-all mb-2 md:mx-5 mx-3"
+      >
+        <span className="text-xl pt-2 block">View Slide</span>
+      </LinkTo>
+      <Text p>
+        A 65-year-old patient diagnosed with lung cancer shares insights into
+        coping strategies and navigating life with a cancer diagnosis.
+      </Text>
+
+      <Text p>
+        This case study delves into the life of a 65-year-old patient diagnosed
+        with lung cancer, providing valuable insights into coping strategies and
+        navigating life with a cancer diagnosis. Key aspects covered in this
+        case study include:
+        <List>
+          <li>
+            <strong>Coping Strategies:</strong> Exploration of the coping
+            strategies adopted by the patient to navigate the challenges posed
+            by a lung cancer diagnosis. Insights into emotional resilience,
+            support networks, and maintaining quality of life.
+          </li>
+          <li>
+            <strong>Navigating Life:</strong> A narrative that explores how the
+            patient actively navigates various aspects of life post-diagnosis,
+            including daily routines, social interactions, and participation in
+            meaningful activities.
+          </li>
+          <li>
+            <strong>Emotional Support:</strong> Recognition of the importance of
+            emotional support in the patient's journey, with a focus on the role
+            of family, friends, and healthcare professionals in providing
+            encouragement and comfort.
+          </li>
+        </List>
+      </Text>
+
+      <Text p>
+        Gaining valuable insights into the life of this 65-year-old patient
+        diagnosed with lung cancer provides a deeper understanding of the
+        emotional and practical dimensions of living with a cancer diagnosis.
+        The case study encourages empathy, support, and the cultivation of a
+        compassionate healthcare environment.
+      </Text>
+    </PageLayout>
+  );
+};
+
+export default Article;
